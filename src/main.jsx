33b987d29532762703079326aa600bc9bfd53841
Elite <apipath90@gmail.com>
2025-09-02 14:22:25 +0700

    v1

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/keng",
+    element: (
+      <>
+        <App />
+        <App />
+         <App />
+      </>
+    ),
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  //   <App />
+  //   <App />
+  // </StrictMode>
+  <RouterProvider router={router} />
+);
